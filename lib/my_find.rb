580fbc337449d635a5fac8be1a0cca088e378334
Derric Fragosa <fderric@gmail.com>
2019-10-20 06:56:06 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0 
+  block_return_values = [] 
+  while i < collection.length 
+  yield(collection[i])
+  block_return_values << collection[i]
+  i +=1 
+  end
+    if block_return_values.find
+end 
 
-end
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 && i % 5 == 0 }
